54ae2f2174a4bdb5a37482675cc0f844705e8772
Adrian Statescu <mergesortv@gmail.com>
2025-02-05 07:24:55 +0200

    layoyt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,17 @@
 import './globals.css'
+import type { Metadata } from 'next'
+import { ReactNode } from 'react'
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Slideshow App',
   description: 'A simple slideshow built with Next.js and Tailwind CSS',
 }
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: ReactNode
+}) {
   return (
     <html lang="en">
       <body>{children}</body>
